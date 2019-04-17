5f4319d36b518b3f753cd2b913e394879f6b138b
Vitor <vitorlofonseca@yahoo.com.br>
2019-04-16 21:49:18 -0300

    Env file

---
+++ env.sample.js
@@ -0,0 +1,11 @@
+module.exports = {
+  aws: {
+    url: "AWS_API_URL",
+    api: "/URL/API/PATH/"
+  },
+  spotify: {
+    url: "SPOTIFY_API_URL",
+    bearer: "BEARER_TOKEN",
+    market: "[BR, US, ...]"
+  }
+};
